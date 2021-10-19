4a4769d9f58ff1ffb631a58038fe0bfc17b743db
OJ42 <mcdavid42@gmail.com>
2021-10-19 12:04:23 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require('path')
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // When we want to be able to accept JSON.
+
+//show a random fortune when fortune button is pushed
+app.get('/api/fortunes', function (req, res) { 
+  const fortunes = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Dont count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.'
+  ]
+  //randonly select the fortune to display
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+  res.status(200).send(randomFortune);
+})
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style.css'))
+})
+
+const port = process.env.PORT || 4005
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
