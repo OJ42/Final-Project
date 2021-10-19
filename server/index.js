b84716e34d7f326a1e0573acf21f0c624745a7ec
OJ42 <mcdavid42@gmail.com>
2021-10-19 13:21:32 -0600

    image pathing

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.use('/static', express.static(path.join(__dirname, 'content')))
+app.use('/static', express.static('content'))
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../client/index.html'));
